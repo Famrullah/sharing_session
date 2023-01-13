0c287655470036351c4c11ff181d70a3457da884
Faisal Amrullah <faisal.amrullah@kitabisa.com>
2023-01-14 02:39:27 +0700

    feat: refactor

--- async/index.js
+++ async/index.js
@@ -2,7 +2,6 @@ const rootconfig = {
   root: null,
   rootMargin: '0px',
 };
-
 const observerElements = document.querySelectorAll('.scroll-reveal');
 const api = {
   people:'https://swapi.dev/api/people/1',
@@ -13,11 +12,10 @@ const api = {
 const Observer = new IntersectionObserver(
   (entries) => {
     entries.forEach(entry => {
-      console.log(entry)
       if (entry.isIntersecting) {
-        console.log(entry)
-        const id = entry.target.id
-        getData(api[id],id)
+        const {id} = entry.target
+        const classList = entry.target.classList[1]
+        getData(api[id], id , classList)
         Observer.unobserve(entry.target);
       }
     });
@@ -25,45 +23,33 @@ const Observer = new IntersectionObserver(
   rootconfig
 );
 
-
-async function getData(api,id) {
+async function getData(api,id ,classList) {
   const req = await fetch(api)
   const res = await req.json()
-  renderHTML(res,id)
+  renderHTML(res,id, classList)
 }
 
-function peopleUI(data) {
+function peopleComponent(data) {
   return`<div>${data.name}</div>`
 }
 
-function filmsUi(data) {
+function filmsComponent(data) {
   return`<div>${data.title}</div>`
 }
 
-function vehicleUi(data) {
+function vehiclesComponent(data) {
   return`<div>${data.name}</div>`
 }
 
-function renderHTML (data,id) {
-  console.log(data)
-  let container =''
-  if(id == 'people') {
-    container = peopleUI(data)
-    const peopleContainer = document.querySelector('.people-container')
-    peopleContainer.innerHTML = container
+function renderHTML (data,id,classList) {
+  const componentUpdate = {
+    people: () => peopleComponent(data),
+    films: () => filmsComponent(data),
+    vehicle: () => vehiclesComponent(data)
   }
-
-  if(id == 'films'){
-    container = filmsUi(data)
-    const peopleContainer = document.querySelector('.films-container')
-    peopleContainer.innerHTML = container
-  }
-
-  if(id == 'vehicle'){
-    container = vehicleUi(data)
-    const peopleContainer = document.querySelector('.vehicle-container')
-    peopleContainer.innerHTML = container
-  }
+  let container = componentUpdate[id]()
+  let selector = document.querySelector(`.${classList}`)
+  selector.innerHTML = container
 }
 
 observerElements.forEach(el => {
